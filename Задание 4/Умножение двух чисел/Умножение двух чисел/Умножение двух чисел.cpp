4e79600f773029446f3c2bda5d55632a740ef72e
Андрей Елькин <92205512+ElkinAndrey@users.noreply.github.com>
2021-10-27 14:06:41 +0500

    Add files via upload

---
+++ Задание 4/Умножение двух чисел/Умножение двух чисел/Умножение двух чисел.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	// Объявление переменных
+	double i, j, a;
+
+	// Пользователь вводит два числа
+	cout << "Программа, умножает два числа.\n";
+	cout << "Введите первое число : ";
+	cin >> i;
+	cout << "Введите второе число : ";
+	cin >> j;
+
+	// Числа перемножаются 
+	a = i * j;
+
+	// Число выводится на экран с точностью 4 знака полсе запятой
+	cout << fixed << showpoint;
+	cout << "Ответ : " << setprecision(4) << a << endl;
+
+	// Надо
+	system("pause");
+	return 0;
+}
---
+++ Задание 4/Чему равно выражение/Чему равно выражение/Чему равно выражение.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    // Записывается i
+    int i = 1;
+
+    // Находится ответ искомого выражения
+    int ans = ++i * i++;
+
+    // Выводится ответ
+    cout << "Выражение ++i * i++, при i = 1, будет равно : " << ans << endl;
+
+    // Надо
+    system("pause");
+    return 0;
+}
+
+
