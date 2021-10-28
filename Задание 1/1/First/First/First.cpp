aeddc4de969999e74c493252c5e33356cd05be3a
Андрей Елькин <92205512+ElkinAndrey@users.noreply.github.com>
2021-10-28 10:09:06 +0500

    Add files via upload

---
+++ Задание 1/1/First/First/First.cpp
@@ -0,0 +1,49 @@
+﻿/*
+Задача: 
+На ввод поступает число от 100 до 999 включительно:
+Цифры в том числе не должны повторяться.
+Выведите все комбинации числа без повторов.
+Вывести максимальное число из комбинаций
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    int s; // Строка
+    int max = 0;
+    cout << "Введите число от 100 до 999 : ";
+    cin >> s;
+
+    // Массив со всеми значениямиы
+    int first = s / 100;
+    int second = s / 10 % 10;
+    int third = s % 10;
+    int mas[] = {
+        first * 100 + second * 10 + third,
+        first * 100 + third * 10 + second,
+        second * 100 + first * 10 + third,
+        second * 100 + third * 10 + first,
+        third * 100 + first * 10 + second,
+        third * 100 + second * 10 + first,
+    };
+
+    // Вывод вcех значений
+    cout << "Все комбинации числа без повторов : " << endl;
+    for (int i : mas) {
+        cout << i << endl;
+    }
+
+    // Поиск наибольшего
+    for (int i : mas) {
+        if (i > max)
+            max = i;
+    }
+    cout << endl << "Максимальное число из комбинаций : " << max << endl;
+
+    system("pause");
+    return 0;
+}
---
+++ Задание 1/2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    srand(time(0));
+
+    const int value = rand() % 100;
+    int num;
+
+    cout << "Угадайте число в диапазоне от 1 до 100" << endl;
+    for (int i = 1; i <= 6; i++) {
+        cout << "Попытка ввода числа номер " << i <<  " : ";
+        cin >> num;
+
+        if (num == value) {
+            cout << "Вы нашли правильный ответ за количество попыток : " << i << ". Это число : " << value << endl;
+            break;
+        }
+        else if (i == 6)
+            cout << "Вы не нашли число за 6 попыток" << endl;
+        else if (num > value)
+            cout << "Возьмите поменьше" << endl;
+        else if (num < value)
+            cout << "Возьмите побольше" << endl;
+    }
+
+    system("pause");
+    return 0;
+}
