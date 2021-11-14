064058e410b2cdefcd5f88e81a3cf179ee2f9b6d
Андрей Елькин <92205512+ElkinAndrey@users.noreply.github.com>
2021-11-14 19:39:50 +0500

    Add files via upload

---
+++ Лабораторная работа 1/Лабораторная работа 1.1/Лабораторная работа 1.1/Лабораторная работа 1.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	float af = 100.0, bf = 0.001;
+	float answerf1 = (af - bf) * (af - bf) * (af - bf) * (af - bf);
+	float answerf2 = af * af * af * af - 4 * af * af * af * bf;
+	float answerf3 = answerf1 - answerf2;
+	float answerf4 = 6 * af * af * bf * bf - 4 * af * bf * bf * bf + bf * bf * bf * bf;
+	float answerf = answerf3 / answerf4;
+
+	cout << answerf1 << endl << answerf2 << endl << answerf3 << endl << answerf4 << endl << answerf << endl << endl;
+
+	double ad = 100.0, bd = 0.001;
+	double answerd1 = (ad - bd) * (ad - bd) * (ad - bd) * (ad - bd);
+	double answerd2 = ad * ad * ad * ad - 4 * ad * ad * ad * bd;
+	double answerd3 = answerd1 - answerd2;
+	double answerd4 = 6 * ad * ad * bd * bd - 4 * ad * bd * bd * bd + bd * bd * bd * bd;
+	double answerd = answerd3 / answerd4;
+
+	cout << answerd1 << endl << answerd2 << endl << answerd3 << endl << answerd4 << endl << answerd << endl << endl;;
+
+	system("Pause");
+
+	return 0;
+}
---
+++ Лабораторная работа 1/Лабораторная работа 1.2/Лабораторная работа 1.2/Лабораторная работа 1.2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	int n, m, ans1;
+	bool ans2, ans3;
+	cout << "Введите значение числа n : ";
+	cin >> n;
+	cout << "Введите значение числа m : ";
+	cin >> m;
+	ans1 = n++ * m;
+	ans2 = n++ < m;
+	ans3 = m-- > m;
+
+	cout << "Значение выражения n++ * m : " << ans1 << endl;
+	cout << "Значение выражения n++ < m : " << ans2 << endl;
+	cout << "Значение выражения m-- > m : " << ans3 << endl;
+
+	system("Pause");
+	return 0;
+}
